e5599ea2f20663469ddd3fd4c57f9822359265b4
BastienBellariva <bastien.bellariva@ynov.com>
2017-12-15 15:56:59 +0100

    correction Singleton

--- eatmanager-service/src/test/java/TestServicesDatabase.java
+++ eatmanager-service/src/test/java/TestServicesDatabase.java
@@ -3,7 +3,7 @@ import org.junit.Test;
 
 public class TestServicesDatabase
 {
-    private static String IPServer = "172.20.10.9";
+    private static String IPServer = "192.168.1.50";
 
     @Test
     //Test de la migration avec Flyway
@@ -25,10 +25,12 @@ public class TestServicesDatabase
         //Boucle pour Test de conservation de l'instance
         for (int cpt=0; cpt<10; cpt++) {
             try {
-                Jdbi connexion = DatabaseConnexion.getInstance(IPServer).getConnexion();
+                DatabaseConnexion.setIP(IPServer);
+                Jdbi connexion = DatabaseConnexion.getInstance().getConnexion();
             } catch (Exception e) {
                 e.printStackTrace();
             }
+            System.out.println(DatabaseConnexion.getInstance().getUrl());
             System.out.println("Compteur : " + cpt);
         }
     }
